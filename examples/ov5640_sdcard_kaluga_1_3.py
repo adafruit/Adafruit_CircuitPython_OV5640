f65dc3cd2646989c79fe1c4158ffc2ec5804dfd5
Jeff Epler <jepler@gmail.com>
2021-10-07 13:32:28 -0500

    Add sdcard example

---
+++ examples/ov5640_sdcard_kaluga_1_3.py
@@ -0,0 +1,104 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2021 Jeff Epler for Adafruit Industries
+#
+# SPDX-License-Identifier: Unlicense
+
+"""
+The Kaluga development kit comes in two versions (v1.2 and v1.3); this demo is
+tested on v1.3.
+
+The audio board must be mounted between the Kaluga and the LCD, it provides the
+I2C pull-ups(!)
+
+This example also requires an SD card breakout wired as follows:
+ * IO18: SD Clock Input
+ * IO17: SD Serial Output (MISO)
+ * IO14: SD Serial Input (MOSI)
+ * IO12: SD Chip Select
+
+Insert a CircuitPython-compatible SD card before powering on the Kaluga.
+Press the "Record" button on the audio daughterboard to take a photo.
+"""
+
+import os
+
+import time
+import analogio
+import board
+import busio
+import adafruit_ov5640
+import neopixel
+import storage
+import sdcardio
+
+V_MODE = 1.98
+V_RECORD = 2.41
+
+a = analogio.AnalogIn(board.IO6)
+
+pixel = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.3, auto_write=False)
+
+bus = busio.I2C(scl=board.CAMERA_SIOC, sda=board.CAMERA_SIOD)
+cam = adafruit_ov5640.OV5640(
+    bus,
+    data_pins=board.CAMERA_DATA,
+    clock=board.CAMERA_PCLK,
+    vsync=board.CAMERA_VSYNC,
+    href=board.CAMERA_HREF,
+    mclk=board.CAMERA_XCLK,
+    mclk_frequency=24_000_000,
+    size=adafruit_ov5640.OV5640_SIZE_QSXGA,
+)
+
+sd_spi = busio.SPI(clock=board.IO18, MOSI=board.IO14, MISO=board.IO17)
+sd_cs = board.IO12
+sdcard = sdcardio.SDCard(sd_spi, sd_cs)
+vfs = storage.VfsFat(sdcard)
+storage.mount(vfs, "/sd")
+
+
+def exists(filename):
+    try:
+        os.stat(filename)
+        return True
+    except OSError as e:
+        return False
+
+
+_image_counter = 0
+
+def open_next_image():
+    global _image_counter
+    while True:
+        filename = f"/sd/img{_image_counter:04d}.jpg"
+        _image_counter += 1
+        if exists(filename):
+            continue
+        print("# writing to", filename)
+        return open(filename, "wb")
+
+cam.colorspace = adafruit_ov5640.OV5640_COLOR_JPEG
+cam.quality = 7
+b = bytearray(cam.capture_buffer_size)
+
+print("Press 'record' button to take a JPEG image")
+while True:
+    pixel[0] = 0x0000ff; pixel.write()
+    a_voltage = a.value * a.reference_voltage / 65535
+    record_pressed = abs(a_voltage - V_RECORD) < 0.05
+    if record_pressed:
+        pixel[0] = 0xff0000; pixel.write(); time.sleep(.01)
+        jpeg = cam.capture(b)
+        print(f"Captured {len(jpeg)} bytes of jpeg data (had allocated {cam.capture_buffer_size} bytes")
+        print(f"Resolution {cam.width}x{cam.height}")
+        try:
+            pixel[0] = 0x00ff00; pixel.write()
+            with open_next_image() as f:
+                f.write(jpeg)
+            print("# Wrote image")
+            pixel[0] = 0x000000; pixel.write()
+        except OSError as e:
+            print(e)
+        while record_pressed:
+            a_voltage = a.value * a.reference_voltage / 65535
+            record_pressed = abs(a_voltage - V_RECORD) < 0.05
